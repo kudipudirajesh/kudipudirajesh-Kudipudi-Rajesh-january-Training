86b40ce6875dcee2e704a3b2b44404cc05983cf2
kudipudirajesh <143701782+kudipudirajesh@users.noreply.github.com>
2026-01-17 20:54:22 +0530

    folder name changed

---
+++ Assignment1/Q1.py
@@ -0,0 +1,8 @@
+def count_message(msg, count=0):
+    count += 1
+    print(f"Message: {msg}, Count: {count}")
+    return count
+
+count_message("heya")
+count_message("hello")
+count_message("again")
