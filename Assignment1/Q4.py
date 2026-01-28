de335f96a2a5b20b9cdb9a7a1b18fb4f1c34774b
kudipudirajesh <143701782+kudipudirajesh@users.noreply.github.com>
2026-01-28 22:22:36 +0530

    assignment-3

--- Assignment1/Q4.py
+++
@@ -1,8 +0,0 @@
-def remove_even(numbers):
-    for n in list(numbers): 
-        if n % 2 == 0:
-            numbers.remove(n)
-    return numbers
-
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(remove_even(nums))
