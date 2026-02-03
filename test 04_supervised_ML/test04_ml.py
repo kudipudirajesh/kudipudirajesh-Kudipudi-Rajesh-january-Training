dd74d60971ad3961119f8adbe36ef0744c2e3641
kudipudirajesh <143701782+kudipudirajesh@users.noreply.github.com>
2026-02-03 12:23:05 +0530

    updated  data preprocessing code

--- test 04_supervised_ML/test04_ml.py
+++ test 04_supervised_ML/test04_ml.py
@@ -41,9 +41,3 @@ df = pd.get_dummies(df, drop_first=True)
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-X = df.drop("target", axis=1)
-y = df["target"]
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
